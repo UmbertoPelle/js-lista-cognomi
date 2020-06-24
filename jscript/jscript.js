9c18db0ecdca0200f94f2a908e77ddf810b12615
Umberto <umbertopellegrino96@gmail.com>
2020-06-24 15:43:50 +0200

    Controllo se il cognome è valido

--- jscript/jscript.js
+++ jscript/jscript.js
@@ -1,18 +1,23 @@
-//chiedete all'utente un cognome e aggiungetelo alla lista
-var posizione = 0;
-var cognomi = ["pellegrino", "maradei", "cane", "gatto"];
-var cognomeUtente = (prompt("Inserisci il tuo cognome :"));
-cognomi.push(cognomeUtente);
-console.log(cognomi);
+document.getElementById("btnRegistrati").addEventListener("click", function () {
+  //chiedete all'utente un cognome e aggiungetelo alla lista
+  var posizione = 0;
+  var cognomi = ["pellegrino", "maradei", "cane", "gatto"];
+  var cognomeUtente = document.getElementById("cognomeUtente").value;
 
-//poi ordinate
-cognomi=cognomi.sort();
-console.log(cognomi);
-
-//l'esercizio chiede l'indice del nuovo elemento inserito
-for (var i = 0; i < cognomi.length; i++) {
-  if (cognomi[i] == cognomeUtente) {
-    posizione = i
+  while (cognomeUtente === "") {
+    cognomeUtente = prompt("Attenzione, valore non valido riprovare:");
+    cognomeUtente != "";
   }
-}
-console.log(posizione);
+  cognomi.push(cognomeUtente);
+
+  //poi ordinate
+  cognomi=cognomi.sort();
+
+  //l'esercizio chiede l'indice del nuovo elemento inserito
+  for (var i = 0; i < cognomi.length; i++) {
+    if (cognomi[i] == cognomeUtente) {
+      posizione = i
+    }
+  }
+  document.getElementById("risPosizione").innerHTML="Benvenuto, sei stato inserito in posizione " + ++posizione;
+});
