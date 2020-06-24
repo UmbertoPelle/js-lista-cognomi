ee9947ce004494ab8d5dbc130a50c4b22f21d80f
Umberto <umbertopellegrino96@gmail.com>
2020-06-24 15:18:34 +0200

    trova posizione

--- jscript/jscript.js
+++ jscript/jscript.js
@@ -1,4 +1,5 @@
 //chiedete all'utente un cognome e aggiungetelo alla lista
+var posizione = 0;
 var cognomi = ["pellegrino", "maradei", "cane", "gatto"];
 var cognomeUtente = (prompt("Inserisci il tuo cognome :"));
 cognomi.push(cognomeUtente);
@@ -9,3 +10,9 @@ cognomi=cognomi.sort();
 console.log(cognomi);
 
 //l'esercizio chiede l'indice del nuovo elemento inserito
+for (var i = 0; i < cognomi.length; i++) {
+  if (cognomi[i] == cognomeUtente) {
+    posizione = i
+  }
+}
+console.log(posizione);
